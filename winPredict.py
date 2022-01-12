b4db248e4e9290db59a7a87d9be8665630010320
Medokins <nikodemszwast@gmail.com>
2022-01-12 22:27:25 +0100

    select more accurate model

--- winPredict.py
+++ winPredict.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import sklearn
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from getDataApi import getData
 
 mainSummonerName = "Aaron II"
-liveGame = False
+liveGame = True
 
 df = pd.read_csv("datasets/LeaugeOfLegends.csv")
 df = df.drop('gameId', axis=1)
@@ -32,18 +32,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 neverSeenData = [0,6,6,6,0,0,0,0,16400,7.0,210,-1004,21.0,1640.0,1,6,6,7,1,1,0,0,17404,7.0,225,1004,22.5,1740.4]
 
-random_forest = RandomForestClassifier(n_estimators=100)
-random_forest.fit(X_train, y_train)
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
 
 if liveGame:
     gameData = getData(mainSummoner=mainSummonerName, verboose=True)
     print(gameData[0])
-    if random_forest.predict([gameData[1:]]) == 1:
+    if logreg.predict([gameData[1:]]) == 1:
         print("blue team will most likely win")
     else:
         print("red team will most likely win")
 else:
-    if random_forest.predict([neverSeenData]) == 1:
+    if logreg.predict([neverSeenData]) == 1:
         print("blue team will most likely win")
     else:
         print("red team will most likely win")
