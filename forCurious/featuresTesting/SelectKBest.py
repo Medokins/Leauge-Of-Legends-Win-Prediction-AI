31c88e53c8ba908fc4323189dd5f4bc2dc1fd116
Medokins <nikodemszwast@gmail.com>
2022-01-14 17:14:11 +0100

    minor improvements

--- forCurious/featuresTesting/SelectKBest.py
+++ forCurious/featuresTesting/SelectKBest.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-from sklearn import preprocessing
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 
--- forCurious/featuresTesting/TreeBasedClassifier.py
+++ forCurious/featuresTesting/TreeBasedClassifier.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.ensemble import ExtraTreesClassifier
 import matplotlib.pyplot as plt
 
