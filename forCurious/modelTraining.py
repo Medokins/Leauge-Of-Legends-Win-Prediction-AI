2301579593b817b6a28e6d90e5794c6841ea87f3
Medokins <nikodemszwast@gmail.com>
2022-01-14 17:08:53 +0100

    do better file organization

--- forCurious/modelTraining.py
+++ forCurious/modelTraining.py
@@ -31,4 +31,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
 
-pickle.dump(logreg, open('model.pkl', 'wb'))
+pickle.dump(logreg, open('../model.pkl', 'wb'))
